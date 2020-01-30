0abd286c1ce947bc152876218a1c5b3bfe8c18be
danilrespati <taufiq.dhanil@gmail.com>
2020-01-30 11:51:27 -0700

    calibration

--- program/static/calPos.py
+++ program/static/calPos.py
@@ -1,46 +1,72 @@
-import numpy as np
+from imutils import paths
+import imutils
 import cv2
-width = 1280
-height = 720
-cap = cv2.VideoCapture(0)
-cap.set(3,width) # set Width
-cap.set(4,height) # set Height
-cap.set(10,0.6)    
-ok, img = cap.read()
-frame = cv2.flip(img, -1)
-bbox = (112, 50, 88, 126)
-tu = (bbox[0],bbox[1])
-wa = ((bbox[0]+bbox[2]),bbox[1])
-ga = (bbox[0],(bbox[1]+bbox[3]))
-pa = ((bbox[0]+bbox[2]),(bbox[1]+bbox[3]))
-ppm = ((bbox[2]/0.2)+(bbox[3]/0.3))/2 # actual box 20x30cm
-# Uncomment to calibrate
-# bbox = cv2.selectROI(frame, False)
-print(bbox)
-print(tu)
-print(wa)
-print(ga)
-print(pa)
-print(ppm)
-while(True):
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, -1) # Flip camera vertically
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Middle line
-    cv2.line(frame, (640,0), (640,height), (0,0,255), 4)
-    # Indicator
-    cv2.line(frame, (bbox[0],0), (bbox[0],height), (255,0,0), 2)
-    cv2.line(frame, ((bbox[0]+bbox[2]),0), ((bbox[0]+bbox[2]),height), (0,255,0), 2)
-    cv2.line(frame, (0,bbox[1]), (width,bbox[1]), (0,0,255), 2)
-    cv2.line(frame, (0,(bbox[1]+bbox[3])), (width,(bbox[1]+bbox[3])), (0,0,0), 2)
-    # Coordinate arrow
-    cv2.line(frame, (0,0), (int(width/20),int(height/20)), (0,0,255), 2)
-    cv2.line(frame, (int((width/20)-15),int(height/20)), (int(width/20),int(height/20)), (0,0,255), 2)
-    cv2.line(frame, (int(width/20),int((height/20)-15)), (int(width/20),int(height/20)), (0,0,255), 2)
-    cv2.imshow('frame', frame)
-    
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: # press 'ESC' to quit
-        break
-cap.release()
+import os
+import numpy as np
+
+def initCam():
+	global frameWidth
+	global frameHeight
+	frameWidth = 1280
+	frameHeight = 720
+	brightness = 0.6
+	cam = cv2.VideoCapture(0)
+	cam.set(3, frameWidth)
+	cam.set(4, frameHeight)
+	cam.set(10, brightness)
+	return cam
+
+def findMarker(frame):
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	blur = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(blur, 35, 125)
+	cnts = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	c = max(cnts, key = cv2.contourArea)
+	return cv2.minAreaRect(c)
+	
+def distanceToCamera(knownHeight, focalLength, perHeight):
+	return (knownHeight * focalLength) / perHeight
+	
+def pixelPerMetric(height, perKnownHeight):
+	return (height / perKnownHeight)
+
+cam = initCam()
+flag = 0
+
+image = cv2.imread("/home/pi/skripsi/data/cal2mStatic.png")
+cv2.imshow('Preview',image)
+knownDistance = 200
+knownHeight = 0.5
+marker = findMarker(image)
+focalLength = (marker[1][1] * knownDistance) / knownHeight
+
+while True:
+	ret, frame = cam.read()
+	frame = cv2.flip(frame, -1)
+	#cv2.line(frame, (int(frameWidth/2),0), (int(frameWidth/2),frameHeight), (0,0,255), 4)
+	while(flag==0):
+		roi = cv2.selectROI(frame)
+		flag = 1
+	frame = frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+	marker = findMarker(frame)
+	distance = distanceToCamera(knownHeight, focalLength, marker[1][1])
+	ppm = pixelPerMetric(marker[1][1], knownHeight)
+	box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+	box = np.int0(box)
+	cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
+	cv2.putText(frame, "{0} {1}".format(round(distance,2), round(ppm,2)),
+		(0, image.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX,
+		1.0, (0, 255, 0), 3)
+	cv2.imshow('Preview',frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord('q'):
+		flag = 0
+	if key == 27:
+		print(distance)
+		print(ppm)
+		break
+		
+print("\n [INFO] Exiting Program and cleanup stuff")
 cv2.destroyAllWindows()
+cam.release()
