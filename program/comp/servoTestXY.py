22f3ddd8ca05fb683a5658885b4cd2bdeca0c7d1
danilrespati <taufiq.dhanil@gmail.com>
2020-02-06 14:47:12 +0700

    servo test

--- program/comp/servoTestXY.py
+++ program/comp/servoTestXY.py
@@ -1,31 +1,27 @@
 from time import sleep
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-tilt = 17
-pan = 27
+tilt = 11
+pan = 13
 
 GPIO.setup(tilt, GPIO.OUT)  # white => TILT
 GPIO.setup(pan, GPIO.OUT)  # gray ==> PAN
 
-
-def setServoAngle(servo, ds):
+def setServoAngle(servo, dutyCycle):
     pwm = GPIO.PWM(servo, 50)
     pwm.start(8)
-    dutyCycle = ds
     pwm.ChangeDutyCycle(dutyCycle)
     sleep(0.4)
     pwm.stop()
 
-
 if __name__ == '__main__':
-    for i in range(5, 12, 1):
-        print(i)
-        setServoAngle(pan, i)
-        setServoAngle(tilt, i)
+    for dc in range(5, 11, 1):
+        print(dc)
+        setServoAngle(pan, dc)
+        setServoAngle(tilt, dc)
         sleep(0.5)
-
     setServoAngle(pan, 8)
     setServoAngle(tilt, 7)
     GPIO.cleanup()
