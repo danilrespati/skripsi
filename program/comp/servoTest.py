876ebe45d2871134eb46a7d19bc5ed29f1e12eea
danilrespati <taufiq.dhanil@gmail.com>
2020-02-10 16:52:18 +0700

    add initUrl

--- program/comp/servoTest.py
+++ program/comp/servoTest.py
@@ -2,10 +2,15 @@ from time import sleep
 import RPi.GPIO as GPIO
 import numpy as np
 
-testPin = 11
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(testPin, GPIO.OUT)
+def initUrl():
+    url = 'http://192.168.100.13/skripsi/target.php'
+    web = urllib.request.urlopen(url)
+    html = web.read()
+    soup = BeautifulSoup(html, 'lxml')
+    target = soup.find('td', {'id': 'target'}).text
+    anglePan = soup.find('td', {'id': 'x'}).text
+    angleTlt = soup.find('td', {'id': 'y'}).text
+    return target, anglePan, angleTlt
 
 def setServoAngle(servo, angle):
     dutyCycle = round(((7*angle)+1350)/180, 1)
@@ -16,6 +21,11 @@ def setServoAngle(servo, angle):
         print("{0} -> {1}".format(angle, dutyCycle))
         sleep(0.5)
         pwm.stop()
+
+testPin = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(testPin, GPIO.OUT)
 
 if __name__ == '__main__':
     for angle in np.arange(-90, 190, 10):
