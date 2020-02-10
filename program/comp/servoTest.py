86a7d1dfab980bf768738005d43ab9f51b5fd794
danilrespati <taufiq.dhanil@gmail.com>
2020-02-10 15:49:59 +0700

    add servoTest.py

---
+++ program/comp/servoTest.py
@@ -0,0 +1,22 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import numpy as np
+
+
+testPin = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(testPin, GPIO.OUT)  # white => TILT
+
+def setServoAngle(servo, dutyCycle):
+    pwm = GPIO.PWM(servo, 50)
+    pwm.start(0)
+    pwm.ChangeDutyCycle(dutyCycle)
+    sleep(0.2)
+    pwm.stop()
+
+if __name__ == '__main__':
+    for dc in np.arange(5, 11, 0.5):
+        print(dc)
+        setServoAngle(testPin, dc)
+    GPIO.cleanup()
