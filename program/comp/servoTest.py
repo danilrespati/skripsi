7844adbd92749a16afa29900efdfdac32cd1d280
danilrespati <taufiq.dhanil@gmail.com>
2020-02-10 16:37:54 +0700

    DC Limit

--- program/comp/servoTest.py
+++ program/comp/servoTest.py
@@ -8,13 +8,14 @@ GPIO.setwarnings(False)
 GPIO.setup(testPin, GPIO.OUT)
 
 def setServoAngle(servo, angle):
-    pwm = GPIO.PWM(servo, 50)
-    pwm.start(0)
     dutyCycle = round(((7*angle)+1350)/180, 1)
-    pwm.ChangeDutyCycle(dutyCycle)
-    print("{0} -> {1}".format(angle, dutyCycle))
-    sleep(0.5)
-    pwm.stop()
+    if (dutyCycle >= 4.5 and dutyCycle <= 10.5):
+        pwm = GPIO.PWM(servo, 50)
+        pwm.start(0)
+        pwm.ChangeDutyCycle(dutyCycle)
+        print("{0} -> {1}".format(angle, dutyCycle))
+        sleep(0.5)\
+        pwm.stop()
 
 if __name__ == '__main__':
     for angle in np.arange(-90, 190, 10):
