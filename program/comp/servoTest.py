bb80dea5303d2fb5e6e5bd7ac972fca70efb01e1
danilrespati <taufiq.dhanil@gmail.com>
2020-02-10 16:33:39 +0700

    test

--- program/comp/servoTest.py
+++ program/comp/servoTest.py
@@ -7,16 +7,17 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(testPin, GPIO.OUT)
 
-def setServoAngle(servo, dutyCycle):
+def setServoAngle(servo, angle):
     pwm = GPIO.PWM(servo, 50)
     pwm.start(0)
+    dutyCycle = round(((7*angle)+1350)/180, 1)
     pwm.ChangeDutyCycle(dutyCycle)
-    sleep(1)
+    print("{0} -> {1}".format(angle, dutyCycle))
+    sleep(0.5)
     pwm.stop()
 
 if __name__ == '__main__':
-    for dc in np.arange(4, 12, 1):
-        print(dc)
-        setServoAngle(testPin, dc)
-    setServoAngle(testPin, 7.5)
+    for angle in np.arange(-90, 190, 10):
+        setServoAngle(testPin, angle)
+    setServoAngle(testPin, 0)
     GPIO.cleanup()
