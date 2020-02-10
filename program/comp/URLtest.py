8dfc4819d813d3e8d925bfa95783aa781d12d567
danilrespati <taufiq.dhanil@gmail.com>
2020-02-10 17:34:41 +0700

    test

--- program/comp/URLtest.py
+++ program/comp/URLtest.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 def initUrl():
+    url = 'http://192.168.100.13/skripsi/target.php'
+    web = urllib.request.urlopen(url)
     html = web.read()
     soup = BeautifulSoup(html, 'lxml')
     stat = soup.find('em').text
@@ -11,8 +13,6 @@ def initUrl():
     angleTlt = soup.find('td', {'id': 'y'}).text
     return stat, target, anglePan, angleTlt
 
-url = 'http://192.168.100.13/skripsi/target.php'
-web = urllib.request.urlopen(url)
 stat, target, anglePan, angleTlt = initUrl()
 print(stat, target, anglePan, angleTlt)
 while (stat == "Running"):
