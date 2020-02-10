6b6d3cc45d921c6c5b3fbd92e2cf8d6570f01f02
danilrespati <taufiq.dhanil@gmail.com>
2020-02-10 17:12:10 +0700

    test

--- program/comp/URLtest.py
+++ program/comp/URLtest.py
@@ -1,6 +1,20 @@
+from time import sleep
 from bs4 import BeautifulSoup
 import urllib.request
 
+class url:
+    def __init__(self, url):
+        self.web = urllib.request.urlopen(url)
+
+    def parse(self):
+        html = web.read()
+        soup = BeautifulSoup(html, 'lxml')
+        stat = soup.find('em').text
+        target = soup.find('td', {'id': 'target'}).text
+        anglePan = soup.find('td', {'id': 'x'}).text
+        angleTlt = soup.find('td', {'id': 'y'}).text
+        return stat, target, anglePan, angleTlt
+        
 def initUrl():
     url = 'http://192.168.100.13/skripsi/target.php'
     web = urllib.request.urlopen(url)
@@ -12,6 +26,8 @@ def initUrl():
     angleTlt = soup.find('td', {'id': 'y'}).text
     return stat, target, anglePan, angleTlt
 
-stat, target, anglePan, angleTlt = initUrl()
+com = url('http://192.168.100.13/skripsi/target.php')
+stat, target, anglePan, angleTlt = url.parse()
 while (stat == "Running"):
+    stat, target, anglePan, angleTlt = url.parse()
     print(target, anglePan, angleTlt)
