3cd6368fd99b64abb5cdd77cca42a1c3e1b9d47e
danilrespati <taufiq.dhanil@gmail.com>
2020-02-11 23:30:28 +0700

    new formula

--- program/dynamic/angleServoCtrl.py
+++ program/dynamic/angleServoCtrl.py
@@ -3,19 +3,21 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-def setServoAngle(servoPin, angle):
-	pwm = GPIO.PWM(servoPin, 50)
+def setServoAngle(servo, angle):
+	pwm = GPIO.PWM(servo, 50)
 	pwm.start(8)
 	#dutyCycle = ((7*angle)+1350)/180
 	#dutyCycle = angle / 18. + 3.
-	dutyCycle = angle / 10
+	#dutyCycle = angle / 10
+	dutyCycle = ((angle*-1)+126) / 18
 	pwm.ChangeDutyCycle(dutyCycle)
 	sleep(0.3)
 	pwm.stop()
 
 if __name__ == '__main__':
 	import sys
-	servoPin = int(sys.argv[1])
-	GPIO.setup(servoPin, GPIO.OUT)
-	setServoAngle(servoPin, int(sys.argv[2]))
+	servo = int(sys.argv[1])
+	angle = int(sys.argv[2])
+	GPIO.setup(servo, GPIO.OUT)
+	setServoAngle(servo, angle)
 	GPIO.cleanup()
