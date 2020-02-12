cdb063b5964cbdb7e7a57217eab0280b8cd0d271
danilrespati <taufiq.dhanil@gmail.com>
2020-02-12 17:14:32 +0700

    test

--- program/dynamic/test.py
+++ program/dynamic/test.py
@@ -2,4 +2,4 @@ import subprocess
 import os
 servo = str(13)
 angle = str(50)
-subprocess.run(['/home/pi/skripsi/program/dynamic/angleServoCtrl.py', servo, angle])
+subprocess.call(['./abc.py', servo, angle])
