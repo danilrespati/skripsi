84c17cf0752498319604c57f99337191698f8d6e
danilrespati <taufiq.dhanil@gmail.com>
2020-02-12 17:31:20 +0700

    test

--- program/dynamic/test.py
+++ program/dynamic/test.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 def moveServo(servo, angle):
-    #os.system("python angleServoCtrl.py " + str(servo) + " " + str(angle))
+    os.system("python angleServoCtrl.py " + str(servo) + " " + str(angle))
     print("[INFO] Positioning servo at GPIO {0} to {1} degrees\n".format(servo, angle))
 
 moveServo(13, 50)
