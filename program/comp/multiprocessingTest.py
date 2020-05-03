b3b156758fd601ffc7011d288d084dd4c096bffd
danilrespati <taufiq.dhanil@gmail.com>
2020-05-04 03:34:32 +0700

    test

--- program/comp/multiprocessingTest.py
+++ program/comp/multiprocessingTest.py
@@ -2,7 +2,7 @@ from multiprocessing import Process, Manager, Value
 import socket
 import time
 
-def server(msg):
+def server(waktu):
     while True:
         msg = "The time is {0}".format(time.time())
         clientsocket, address = s.accept()
