5164d0ea6338a13c767d177ad0020e7669ef2b9d
danilrespati <taufiq.dhanil@gmail.com>
2020-05-04 01:57:26 +0700

    test time

--- program/comp/server.py
+++ program/comp/server.py
@@ -1,10 +1,12 @@
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("192.168.0.120", 1234))
 s.listen(5)
 
 while True:
+    msg = "The time is {0}".format(time.time())
     clientsocket, address = s.accept()
     print("Connection from {0} has been established!".format(address))
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    clientsocket.send(bytes(msg, "utf-8"))
