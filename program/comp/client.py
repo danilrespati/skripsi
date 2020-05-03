73b370d8490ee60bba26121d053df9e2b69d388c
danilrespati <taufiq.dhanil@gmail.com>
2020-05-04 01:45:38 +0700

    fix lagi

--- program/comp/client.py
+++ program/comp/client.py
@@ -1,7 +1,7 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket)
-s.connect(192.168.0.120, 1234)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect("192.168.0.120", 1234)
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
--- program/comp/server.py
+++ program/comp/server.py
@@ -1,6 +1,6 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind("192.168.0.120", 1234)
 s.listen(5)
 
