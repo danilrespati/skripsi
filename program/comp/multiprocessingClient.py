3420ffc024e57244c16ce3d7ec72728dedad14cb
danilrespati <taufiq.dhanil@gmail.com>
2020-05-04 16:18:10 +0700

    test

--- program/comp/multiprocessingClient.py
+++ program/comp/multiprocessingClient.py
@@ -1,10 +1,9 @@
 import socket
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.0.120", 1234))
-
 for i in range(5):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("192.168.0.120", 1234))
     msg = s.recv(1024)
     print(msg.decode("utf-8"))
     time.sleep(1)
