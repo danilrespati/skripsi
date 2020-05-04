578802965de12150229f3d48a69e02127317fb26
danilrespati <taufiq.dhanil@gmail.com>
2020-05-04 23:25:11 +0700

    add pickle client

--- program/comp/client.py
+++ program/comp/client.py
@@ -1,7 +1,8 @@
 import socket
+import pickle
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.0.120", 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+msg = s.recv(256)
+print(pickle.loads(msg))
