9008ae8ad323b9cfd1c723c37039b808a9aee944
danilrespati <taufiq.dhanil@gmail.com>
2020-05-06 23:24:18 +0700

    test

--- program/comp/client.py
+++ program/comp/client.py
@@ -1,16 +1,15 @@
 from multiprocessing import Process, Manager, Value
 import socket
 import time
-import signal
-import sys
-import cv2
-import os
-import numpy as np
-import math
 import pickle
 
+start = time.time()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.0.120", 1234))
 
 msg = s.recv(512)
+print(sys.getsizeof(pickle.loads(msg)))
+print(pickle.loads(msg))
 print(msg)
+finish = time.time()
+print(finish-start)
