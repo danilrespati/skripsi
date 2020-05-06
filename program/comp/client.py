22575fc750776e25d6626cc9e3da5fa583ff20b4
danilrespati <taufiq.dhanil@gmail.com>
2020-05-06 23:25:44 +0700

    test

--- program/comp/client.py
+++ program/comp/client.py
@@ -2,6 +2,7 @@ from multiprocessing import Process, Manager, Value
 import socket
 import time
 import pickle
+import sys
 
 start = time.time()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
