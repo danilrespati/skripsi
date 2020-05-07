10d78b262f0f24e6e2dd1b1917b2a9b48917e186
danilrespati <taufiq.dhanil@gmail.com>
2020-05-08 01:53:06 +0700

    add angleServoCtrl.py to comp

---
+++ program/comp/angleServoCtrl.py
@@ -0,0 +1,26 @@
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+def setServoAngle(servo, angle):
+	if (-90 <= angle <= 90):
+		pwm = GPIO.PWM(servo, 50)
+		pwm.start(8)
+		#dutyCycle = ((7*angle)+1350)/180
+		#dutyCycle = angle / 18. + 3.
+		#dutyCycle = angle / 10
+		dutyCycle = ((angle*-1)+126) / 18
+		pwm.ChangeDutyCycle(dutyCycle)
+		sleep(0.3)
+		pwm.stop()
+	else:
+		print("Limit: -90 <= angle <= 90")
+
+if __name__ == '__main__':
+	import sys
+	servo = int(sys.argv[1])
+	angle = float(sys.argv[2])
+	GPIO.setup(servo, GPIO.OUT)
+	setServoAngle(servo, angle)
+	GPIO.cleanup()
